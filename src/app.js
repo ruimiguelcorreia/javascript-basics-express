651e3e5f4002e2eba9a7c8b4519a46bd3cdb7e2a
Rui Correia <rmgc31@gmail.com>
2020-01-18 16:13:54 +0000

    stings tests passing

--- src/app.js
+++ src/app.js
@@ -2,16 +2,32 @@ const express = require('express');
 
 const app = express();
 
-const sayHello = require('./lib/strings');
+const {
+  sayHello,
+  uppercase,
+  lowercase,
+  firstCharacter,
+  firstCharacters,
+} = require('./lib/strings');
 
-app.get('/strings/hello/world', (req, res) => {
-  res.status(200);
-  res.send({ result: 'Hello, world!' });
-  // res.status(200).json({result:'Hello, world!'});
+app.get('/strings/hello/:string', (req, res) => {
+  res.status(200).send({ result: sayHello(req.params.string) });
 });
 
-app.get('/strings/upper/hello', (req, res) => {
-  res.status(200).json({ result: 'HELLO' });
+app.get('/strings/upper/:string', (req, res) => {
+  res.json({ result: uppercase(req.params.string) });
+});
+
+app.get('/strings/lower/:string', (req, res) => {
+  res.json({ result: lowercase(req.params.string) });
+});
+
+app.get('/strings/first-characters/:string', (req, res) => {
+  if (req.query.length === undefined) {
+    res.json({ result: firstCharacter(req.params.string) });
+  } else {
+    res.json({ result: firstCharacters(req.params.string, req.query.length) });
+  }
 });
 
 module.exports = app;
